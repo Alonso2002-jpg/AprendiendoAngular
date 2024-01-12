c52306d9cdfd986a26635fbe1928f35d00c91254
Alonso <alonsojorge980313810@hotmail.com>
2024-01-12 16:41:27 +0100

    Creacion de Componente Card Product

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent {
+  productStyle = {
+    width:'70%',
+    border: '1px solid black',
+    padding: '10px',
+    filter: 'sepia(0)'
+  }
+  product = {
+    img: 'assets/imgReloj.jpg',
+    name: 'Reloj',
+    price: 100.0,
+    description: 'Reloj de pared',
+    quantity: 10
+  }
 
+  onMouseEnter = () => {
+    this.productStyle = { ...this.productStyle, filter: 'sepia(0.5)'}
+  }
+
+  onMouseLeave = () => {
+    this.productStyle = { ...this.productStyle, filter: 'sepia(0)'}
+  }
 }
